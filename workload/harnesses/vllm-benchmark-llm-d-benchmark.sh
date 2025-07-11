382872dcfff6a73b835fc46639ec28d9d9c953a9
maugustosilva <maugusto.silva@gmail.com>
2025-07-11 12:54:26 -0400

    [Run] bugfix: allow the specification of flags without args for (#126)
    vllm-benchmark
    
    Signed-off-by: maugustosilva <maugusto.silva@gmail.com>

--- workload/harnesses/vllm-benchmark-llm-d-benchmark.sh
+++ workload/harnesses/vllm-benchmark-llm-d-benchmark.sh
@@ -2,6 +2,6 @@
 mkdir -p "/requests/$LLMDBENCH_HARNESS_STACK_NAME"
 cd /workspace/vllm-benchmark/
 en=$(cat /workspace/${LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME} | yq -r .executable)
-python benchmarks/${en} --$(cat /workspace/${LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME} | grep -v "^executable" | yq -r 'to_entries | map("\(.key)=\(.value)") | join(" --")')  --seed $(date +%s) --save-result > >(tee -a /requests/$LLMDBENCH_HARNESS_STACK_NAME/stdout.log) 2> >(tee -a /requests/$LLMDBENCH_HARNESS_STACK_NAME/stderr.log >&2)
+python benchmarks/${en} --$(cat /workspace/${LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME} | grep -v "^executable" | yq -r 'to_entries | map("\(.key)=\(.value)") | join(" --")' | sed -e 's^=none ^^g')  --seed $(date +%s) --save-result > >(tee -a /requests/$LLMDBENCH_HARNESS_STACK_NAME/stdout.log) 2> >(tee -a /requests/$LLMDBENCH_HARNESS_STACK_NAME/stderr.log >&2)
 export LLMDBENCH_RUN_EXPERIMENT_HARNESS_RC=$?
 exit $LLMDBENCH_RUN_EXPERIMENT_HARNESS_RC

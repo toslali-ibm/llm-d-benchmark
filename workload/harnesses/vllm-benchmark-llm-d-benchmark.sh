dfc0c329e1519e3ebbfae17b039fa70ed4a2793a
maugustosilva <maugusto.silva@gmail.com>
2025-07-11 23:11:02 -0400

    Additional fixes for "parameterless" CLI options on vllm-benchmark (#133)
    Additionally added an example workload (vllm-benchmark) which allows
    "parameter sweep"
    
    Signed-off-by: maugustosilva <maugusto.silva@gmail.com>

--- workload/harnesses/vllm-benchmark-llm-d-benchmark.sh
+++ workload/harnesses/vllm-benchmark-llm-d-benchmark.sh
@@ -2,7 +2,7 @@
 mkdir -p "$LLMDBENCH_HARNESS_RESULTS_DIR"
 cd /workspace/vllm-benchmark/
 en=$(cat /workspace/${LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME} | yq -r .executable)
-python benchmarks/${en} --$(cat /workspace/${LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME} | grep -v "^executable" | yq -r 'to_entries | map("\(.key)=\(.value)") | join(" --")' | sed -e 's^=none ^^g')  --seed $(date +%s) --save-result > >(tee -a $LLMDBENCH_HARNESS_RESULTS_DIR/stdout.log) 2> >(tee -a $LLMDBENCH_HARNESS_RESULTS_DIR/stderr.log >&2)
+python benchmarks/${en} --$(cat /workspace/${LLMDBENCH_RUN_EXPERIMENT_HARNESS_WORKLOAD_NAME} | grep -v "^executable" | yq -r 'to_entries | map("\(.key)=\(.value)") | join(" --")' | sed -e 's^=none ^^g' -e 's^=none$^^g')  --seed $(date +%s) --save-result > >(tee -a $LLMDBENCH_HARNESS_RESULTS_DIR/stdout.log) 2> >(tee -a $LLMDBENCH_HARNESS_RESULTS_DIR/stderr.log >&2)
 export LLMDBENCH_RUN_EXPERIMENT_HARNESS_RC=$?
 find /workspace -name '*.json' -exec mv -t "$LLMDBENCH_HARNESS_RESULTS_DIR"/ {} +
 exit $LLMDBENCH_RUN_EXPERIMENT_HARNESS_RC
